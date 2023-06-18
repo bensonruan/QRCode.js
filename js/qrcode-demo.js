ac8d39f2bf68d16cbb70913228be16c5c1a62814
Benson Ruan <benson_ruan@hotmail.com>
2023-06-18 14:16:10 +1000

    add download and print function

--- js/qrcode-demo.js
+++ js/qrcode-demo.js
@@ -26,10 +26,49 @@ function generateQRCode () {
 		colorDark : foreColor,
 		colorLight : bgColor,
 		correctLevel : QRCode.CorrectLevel[level]
-	});
+	});	
 }
 
+function printQRCode() 
+{
+  var margin = 15;
+  var printSizeWidth = 700;
+  var printSizeHeight = 1000;
+  var size = parseInt($("#size").val());
+  var printOption = $("#print-option").val();
+  var newWin=window.open('','Print-Window');
+  newWin.document.open();
+  var htmlToPrint = '<html>';
+  htmlToPrint += '<style type="text/css" media="print">@page{size:auto;margin:0;} html,body{height:100%;margin:0!important;padding:0!important;overflow:hidden;text-align-center;}</style>';
+  htmlToPrint += '<body onload="window.print()"><div style="width:100%;text-align:center;margin:auto;">'
+  if(printOption=='S'){
+	htmlToPrint += '<div style="display:inline-block;margin:'+margin+'px">';
+	htmlToPrint += $("#qrcode").children( "img" ).prop('outerHTML');
+	htmlToPrint += '</div>';
+  }else{
+	let heightCount = Math.floor(printSizeHeight / (size + margin));
+	let widthCount = Math.floor(printSizeWidth / (size + margin));
+	for (let i = 1; i <= heightCount; i++) {
+		for (let j = 1; j <= widthCount; j++) {
+			htmlToPrint += '<div style="display:inline-block;margin:'+margin+'px">';
+			htmlToPrint += $("#qrcode").children( "img" ).prop('outerHTML');
+			htmlToPrint += '</div>';
+		} 
+	} 
+  }
+  
+  htmlToPrint +='<div></body></html>'; 
+  newWin.document.write(htmlToPrint);
+  newWin.document.close();
+  setTimeout(function(){newWin.close();},10);
+}
 
+$('#qrcode').bind('DOMSubtreeModified', function(){
+	var imgsource = $("#qrcode").children( "img" ).attr('src');
+	if (typeof imgsource  !== "undefined"){
+		$("#download").attr("href", imgsource);
+	}
+  });
 
 $("#url").
   	on("blur", function () {
@@ -65,3 +104,8 @@ $("#foreground-color").
   on("change", function () {
 	generateQRCode();
 });
+
+$("#print").
+  	on("click", function () {
+		printQRCode();
+	});
